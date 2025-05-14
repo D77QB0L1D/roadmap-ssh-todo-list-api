ffcc9e4c9d379c34a04ba8c916a14cd7b05bca3a
D77QB0L1D <almeidadediego@gmail.com>
2025-05-13 21:10:29 -0300

    Commit inicial.

---
+++ src/main/java/ssh/roadmap/projects/TodoListApiApplication.java
@@ -0,0 +1,13 @@
+package ssh.roadmap.projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ssh/roadmap/projects/TodoListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ssh.roadmap.projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
